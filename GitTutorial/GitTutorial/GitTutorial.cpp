66614c51374f811ec7239742c20c911fb225eac4
ren-hao <60211tom@gmail.com>
2017-02-25 01:01:50 +0800

    vers1.5

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -16,7 +16,6 @@ int main(array<System::String ^> ^args)
 {
     int guess;
     do {
-        std::cout << "Choose a number between 1 - 100 :";
         std::cin >> guess;
     } while (!Guess(guess));
     return 0;
